eff63ab799149e9b2b3b67dae8a6a0144df113b7
Gabriel Godoy <gabe.profe@gmail.com>
2022-05-12 01:17:02 -0300

    picos+desktop

--- logic.js
+++ logic.js
@@ -1,6 +1,13 @@
 const botaoMenu = document.querySelector('.cabecalho__menu');
 const menu = document.querySelector('.menu-lateral');
+const menuLink = document.querySelectorAll(".menu-lateral__link");
 
 botaoMenu.addEventListener("click", function(){
   menu.classList.toggle('menu-lateral--ativo')
 })
+
+for (var i = 0; i < menuLink.length; i++){
+  menuLink[i].addEventListener("click", function(){
+    menu.classList.toggle('menu-lateral--ativo')
+  })
+}
